cd8d9d59dada2d17d0bc7a98fb137ae3516aea3c
Pepega <leeeloooda@gmail.com>
2020-06-29 23:21:07 +0300

    add group name unique

--- cache/src/main/java/com/leeloo/esist/cache/entities/GroupEntity.kt
+++ cache/src/main/java/com/leeloo/esist/cache/entities/GroupEntity.kt
@@ -2,9 +2,10 @@ package com.leeloo.esist.cache.entities
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "Groups")
+@Entity(tableName = "Groups", indices = [Index(value = ["group_name"], unique = true)])
 data class GroupEntity(
     @PrimaryKey @ColumnInfo(name = "group_id") val groupId: Long,
     @ColumnInfo(name = "group_name") val groupName: String,
