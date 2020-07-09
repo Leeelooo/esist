928907d65925dec4ea9bbb344ed957ece5132daa
Pepega <leeeloooda@gmail.com>
2020-07-09 15:03:22 +0300

    testing

--- app/src/androidTest/java/com/leeloo/esist/DbTests.kt
+++ app/src/androidTest/java/com/leeloo/esist/DbTests.kt
@@ -8,10 +8,11 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.leeloo.esist.db.EsistDatabase
 import com.leeloo.esist.db.dao.CrossRefDao
 import com.leeloo.esist.db.dao.GroupDao
+import com.leeloo.esist.db.dao.LessonDao
 import com.leeloo.esist.db.dao.MemberDao
-import com.leeloo.esist.db.entity.GroupEntity
-import com.leeloo.esist.db.entity.GroupMemberCrossRef
-import com.leeloo.esist.db.entity.MemberEntity
+import com.leeloo.esist.db.entity.*
+import com.leeloo.esist.group.GroupLocalDataSource
+import com.leeloo.esist.group.GroupLocalDataSourceImpl
 import kotlinx.coroutines.runBlocking
 import org.junit.After
 import org.junit.Before
@@ -22,8 +23,10 @@ import java.io.IOException
 @RunWith(AndroidJUnit4::class)
 class SimpleEntityReadWriteTest {
     private lateinit var crossRefDao: CrossRefDao
+    private lateinit var lessonDao: LessonDao
     private lateinit var memberDao: MemberDao
     private lateinit var groupDao: GroupDao
+    private lateinit var groupLocalDataSource: GroupLocalDataSource
     private lateinit var db: EsistDatabase
 
     @Before
@@ -35,6 +38,8 @@ class SimpleEntityReadWriteTest {
         crossRefDao = db.crossRefDao()
         memberDao = db.memberDao()
         groupDao = db.groupDao()
+        lessonDao = db.lessonDao()
+        groupLocalDataSource = GroupLocalDataSourceImpl(groupDao, lessonDao, memberDao, crossRefDao)
     }
 
     @After
@@ -151,6 +156,203 @@ class SimpleEntityReadWriteTest {
             crossRefDao.insertGroupToMember(crossRef)
             val members = memberDao.getGroupMembers(groupId)
             assert(members[0].memberId == memberId)
+        }
+    }
+
+    @Test
+    fun lessonCorrectness() {
+        runBlocking {
+            val lesson = LessonEntity(
+                lessonColor = 1,
+                homework = null,
+                book = null,
+                finishTime = 2L,
+                startTime = 1L,
+                topicName = "klds",
+                subjectName = "fdsm;"
+            )
+            val lesson2 = LessonEntity(
+                lessonColor = 1,
+                homework = null,
+                book = null,
+                finishTime = 2L,
+                startTime = 1L,
+                topicName = "klds",
+                subjectName = "fdsm;"
+            )
+            val lesson3 = LessonEntity(
+                lessonColor = 1,
+                homework = null,
+                book = null,
+                finishTime = 2L,
+                startTime = 1L,
+                topicName = "klds",
+                subjectName = "fdsm;"
+            )
+            lessonDao.insertLesson(lesson)
+            lessonDao.insertLesson(lesson2)
+            lessonDao.insertLesson(lesson3)
+            assert(lessonDao.getLessons().size == 3)
+        }
+    }
+
+    @Test
+    fun groupLessons() {
+        runBlocking {
+            val lesson = LessonEntity(
+                lessonColor = 1,
+                homework = null,
+                book = null,
+                finishTime = 2L,
+                startTime = 1L,
+                topicName = "klds",
+                subjectName = "fdsm;"
+            )
+            val lesson2 = LessonEntity(
+                lessonColor = 1,
+                homework = null,
+                book = null,
+                finishTime = 2L,
+                startTime = 1L,
+                topicName = "klds",
+                subjectName = "fdsm;"
+            )
+            val lesson3 = LessonEntity(
+                lessonColor = 1,
+                homework = null,
+                book = null,
+                finishTime = 2L,
+                startTime = 1L,
+                topicName = "klds",
+                subjectName = "fdsm;"
+            )
+            val group = GroupEntity(
+                groupName = "m3",
+                groupColor = 1
+            )
+            val lessonId = lessonDao.insertLesson(lesson)
+            val lesson2Id = lessonDao.insertLesson(lesson2)
+            val lesson3Id = lessonDao.insertLesson(lesson3)
+            lesson.lessonId = lessonId
+            lesson2.lessonId = lesson2Id
+            lesson3.lessonId = lesson3Id
+            val groupId = groupDao.insertGroup(group)
+            crossRefDao.insertLessonToGroup(
+                LessonGroupCrossRef(
+                    groupId = groupId,
+                    lessonId = lessonId
+                )
+            )
+            crossRefDao.insertLessonToGroup(
+                LessonGroupCrossRef(
+                    groupId = groupId,
+                    lessonId = lesson3Id
+                )
+            )
+            assert(!lessonDao.getLessons().contains(lesson2))
+        }
+    }
+
+    @Test
+    fun mappingGroupTest() {
+        runBlocking {
+            val group = GroupEntity(
+                groupName = "m323",
+                groupColor = 1
+            )
+            val group1 = GroupEntity(
+                groupName = "m3",
+                groupColor = 2
+            )
+            group.groupId = groupDao.insertGroup(group)
+            group1.groupId = groupDao.insertGroup(group1)
+            val mappedGroups = groupLocalDataSource.getGroups()
+            assert(mappedGroups[0] == group.toGroup() && mappedGroups[1] == group1.toGroup())
+        }
+    }
+
+    @Test
+    fun groupDetailsCorrectness() {
+        val group = GroupEntity(
+            groupName = "m323",
+            groupColor = 1
+        )
+        val members = listOf(
+            MemberEntity(
+                firstName = "Paul",
+                lastName = "Chlonde",
+                emailAddress = "paul@gmail.com",
+                memberColor = 1
+            ),
+            MemberEntity(
+                firstName = "Paul",
+                lastName = "Chlonde",
+                emailAddress = "pau2l@gmail.com",
+                memberColor = 1
+            )
+        )
+        val lessons = listOf(
+            LessonEntity(
+                lessonColor = 1,
+                homework = null,
+                book = null,
+                finishTime = 2L,
+                startTime = 1L,
+                topicName = "klds",
+                subjectName = "fdsm;"
+            ),
+            LessonEntity(
+                lessonColor = 1,
+                homework = null,
+                book = null,
+                finishTime = 2L,
+                startTime = 1L,
+                topicName = "klds",
+                subjectName = "fdsm;"
+            ),
+            LessonEntity(
+                lessonColor = 1,
+                homework = null,
+                book = null,
+                finishTime = 2L,
+                startTime = 1L,
+                topicName = "klds",
+                subjectName = "fdsm;"
+            )
+        )
+        runBlocking {
+            group.groupId = groupDao.insertGroup(group)
+            members.forEach {
+                it.memberId = memberDao.insertMember(it)
+                crossRefDao.insertGroupToMember(
+                    GroupMemberCrossRef(
+                        memberId = it.memberId,
+                        groupId = group.groupId
+                    )
+                )
+            }
+            lessons.forEach {
+                it.lessonId = lessonDao.insertLesson(it)
+                crossRefDao.insertLessonToGroup(
+                    LessonGroupCrossRef(
+                        lessonId = it.lessonId,
+                        groupId = group.groupId
+                    )
+                )
+            }
+            val groupDetails = groupLocalDataSource.getGroupDetails(group.groupId)
+            if (groupDetails == null) assert(false)
+            assert(
+                groupDetails!!.groupName == group.groupName
+                        && groupDetails.groupId == group.groupId
+                        && groupDetails.groupColor == group.groupColor
+            )
+            members.forEach {
+                assert(groupDetails.groupMembers.contains(it.toMember()))
+            }
+            lessons.forEach {
+                assert(groupDetails.groupSchedule.contains(it.toLesson()))
+            }
         }
     }
 
--- app/src/test/java/com/leeloo/esist/StateTest.kt
+++ app/src/test/java/com/leeloo/esist/StateTest.kt
@@ -78,4 +78,288 @@ class StateTest {
         assert(stateToCompare == newState)
     }
 
+    @Test
+    fun reducingDialogStateToDismissState() {
+        val groupViewState = GroupViewState(
+            loading = false,
+            selectedMembers = emptyList(),
+            selectedLessons = emptyList(),
+            lessonsToAdd = listOf(
+                Lesson(
+                    lessonSubject = "english",
+                    lessonTopic = "english",
+                    startTimestamp = 1L,
+                    endTimestamp = 2L,
+                    lessonColor = 1,
+                    book = null,
+                    homework = null
+                )
+            ),
+            membersToAdd = emptyList(),
+            isDialogOpened = true,
+            groupInsertionError = null,
+            isGroupInserted = false,
+            error = null,
+            groups = listOf(
+                Group(groupName = "1", groupColor = 1),
+                Group(groupName = "2", groupColor = 2)
+            )
+        )
+        val groupModelState = GroupModelState.DialogDismiss
+        val newState = groupModelState.reduce(groupViewState)
+        val stateToCompare = GroupViewState(
+            loading = false,
+            selectedMembers = emptyList(),
+            selectedLessons = emptyList(),
+            lessonsToAdd = emptyList(),
+            membersToAdd = emptyList(),
+            isDialogOpened = false,
+            groupInsertionError = null,
+            isGroupInserted = false,
+            error = null,
+            groups = listOf(
+                Group(groupName = "1", groupColor = 1),
+                Group(groupName = "2", groupColor = 2)
+            )
+        )
+        assert(stateToCompare == newState)
+    }
+
+    @Test
+    fun reducingDialogStateToSelectedGroupWithEmptySelection() {
+        val groupViewState = GroupViewState(
+            loading = false,
+            selectedMembers = emptyList(),
+            selectedLessons = emptyList(),
+            lessonsToAdd = listOf(
+                Lesson(
+                    lessonId = 1L,
+                    lessonSubject = "english",
+                    lessonTopic = "english",
+                    startTimestamp = 1L,
+                    endTimestamp = 2L,
+                    lessonColor = 1,
+                    book = null,
+                    homework = null
+                )
+            ),
+            membersToAdd = emptyList(),
+            isDialogOpened = true,
+            groupInsertionError = null,
+            isGroupInserted = false,
+            error = null,
+            groups = listOf(
+                Group(groupName = "1", groupColor = 1),
+                Group(groupName = "2", groupColor = 2)
+            )
+        )
+        val groupModelState = GroupModelState.LessonChecked(1L)
+        val newState = groupModelState.reduce(groupViewState)
+        val stateToCompare = GroupViewState(
+            loading = false,
+            selectedMembers = emptyList(),
+            selectedLessons = listOf(1L),
+            lessonsToAdd = listOf(
+                Lesson(
+                    lessonId = 1L,
+                    lessonSubject = "english",
+                    lessonTopic = "english",
+                    startTimestamp = 1L,
+                    endTimestamp = 2L,
+                    lessonColor = 1,
+                    book = null,
+                    homework = null
+                )
+            ),
+            membersToAdd = emptyList(),
+            isDialogOpened = true,
+            groupInsertionError = null,
+            isGroupInserted = false,
+            error = null,
+            groups = listOf(
+                Group(groupName = "1", groupColor = 1),
+                Group(groupName = "2", groupColor = 2)
+            )
+        )
+        assert(stateToCompare == newState)
+    }
+
+    @Test
+    fun reducingDialogStateToSelectedGroupWithSelectionOfThisType() {
+        val groupViewState = GroupViewState(
+            loading = false,
+            selectedMembers = emptyList(),
+            selectedLessons = listOf(1L),
+            lessonsToAdd = listOf(
+                Lesson(
+                    lessonId = 1L,
+                    lessonSubject = "english",
+                    lessonTopic = "english",
+                    startTimestamp = 1L,
+                    endTimestamp = 2L,
+                    lessonColor = 1,
+                    book = null,
+                    homework = null
+                )
+            ),
+            membersToAdd = emptyList(),
+            isDialogOpened = true,
+            groupInsertionError = null,
+            isGroupInserted = false,
+            error = null,
+            groups = listOf(
+                Group(groupName = "1", groupColor = 1),
+                Group(groupName = "2", groupColor = 2)
+            )
+        )
+        val groupModelState = GroupModelState.LessonChecked(2L)
+        val newState = groupModelState.reduce(groupViewState)
+        val stateToCompare = GroupViewState(
+            loading = false,
+            selectedMembers = emptyList(),
+            selectedLessons = listOf(1L, 2L),
+            lessonsToAdd = listOf(
+                Lesson(
+                    lessonId = 1L,
+                    lessonSubject = "english",
+                    lessonTopic = "english",
+                    startTimestamp = 1L,
+                    endTimestamp = 2L,
+                    lessonColor = 1,
+                    book = null,
+                    homework = null
+                )
+            ),
+            membersToAdd = emptyList(),
+            isDialogOpened = true,
+            groupInsertionError = null,
+            isGroupInserted = false,
+            error = null,
+            groups = listOf(
+                Group(groupName = "1", groupColor = 1),
+                Group(groupName = "2", groupColor = 2)
+            )
+        )
+        assert(stateToCompare == newState)
+    }
+
+    @Test
+    fun reducingDialogStateToUnselectGroup() {
+        val groupViewState = GroupViewState(
+            loading = false,
+            selectedMembers = emptyList(),
+            selectedLessons = listOf(1L, 2L),
+            lessonsToAdd = listOf(
+                Lesson(
+                    lessonId = 1L,
+                    lessonSubject = "english",
+                    lessonTopic = "english",
+                    startTimestamp = 1L,
+                    endTimestamp = 2L,
+                    lessonColor = 1,
+                    book = null,
+                    homework = null
+                )
+            ),
+            membersToAdd = emptyList(),
+            isDialogOpened = true,
+            groupInsertionError = null,
+            isGroupInserted = false,
+            error = null,
+            groups = listOf(
+                Group(groupName = "1", groupColor = 1),
+                Group(groupName = "2", groupColor = 2)
+            )
+        )
+        val groupModelState = GroupModelState.LessonChecked(2L)
+        val newState = groupModelState.reduce(groupViewState)
+        val stateToCompare = GroupViewState(
+            loading = false,
+            selectedMembers = emptyList(),
+            selectedLessons = listOf(1L),
+            lessonsToAdd = listOf(
+                Lesson(
+                    lessonId = 1L,
+                    lessonSubject = "english",
+                    lessonTopic = "english",
+                    startTimestamp = 1L,
+                    endTimestamp = 2L,
+                    lessonColor = 1,
+                    book = null,
+                    homework = null
+                )
+            ),
+            membersToAdd = emptyList(),
+            isDialogOpened = true,
+            groupInsertionError = null,
+            isGroupInserted = false,
+            error = null,
+            groups = listOf(
+                Group(groupName = "1", groupColor = 1),
+                Group(groupName = "2", groupColor = 2)
+            )
+        )
+        assert(stateToCompare == newState)
+    }
+
+
+    @Test
+    fun reducingDialogStateToSelectedGroupWithSelectionOfOtherType() {
+        val groupViewState = GroupViewState(
+            loading = false,
+            selectedMembers = emptyList(),
+            selectedLessons = listOf(1L),
+            lessonsToAdd = listOf(
+                Lesson(
+                    lessonId = 1L,
+                    lessonSubject = "english",
+                    lessonTopic = "english",
+                    startTimestamp = 1L,
+                    endTimestamp = 2L,
+                    lessonColor = 1,
+                    book = null,
+                    homework = null
+                )
+            ),
+            membersToAdd = emptyList(),
+            isDialogOpened = true,
+            groupInsertionError = null,
+            isGroupInserted = false,
+            error = null,
+            groups = listOf(
+                Group(groupName = "1", groupColor = 1),
+                Group(groupName = "2", groupColor = 2)
+            )
+        )
+        val groupModelState = GroupModelState.MemberChecked(3L)
+        val newState = groupModelState.reduce(groupViewState)
+        val stateToCompare = GroupViewState(
+            loading = false,
+            selectedMembers = listOf(   3L),
+            selectedLessons = listOf(1L),
+            lessonsToAdd = listOf(
+                Lesson(
+                    lessonId = 1L,
+                    lessonSubject = "english",
+                    lessonTopic = "english",
+                    startTimestamp = 1L,
+                    endTimestamp = 2L,
+                    lessonColor = 1,
+                    book = null,
+                    homework = null
+                )
+            ),
+            membersToAdd = emptyList(),
+            isDialogOpened = true,
+            groupInsertionError = null,
+            isGroupInserted = false,
+            error = null,
+            groups = listOf(
+                Group(groupName = "1", groupColor = 1),
+                Group(groupName = "2", groupColor = 2)
+            )
+        )
+        assert(stateToCompare == newState)
+    }
+
 }
